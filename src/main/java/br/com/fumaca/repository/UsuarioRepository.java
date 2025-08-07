6c0c241f0325bc978f21445244ddadeda9cfa3ea
LucasRBeckhauser <lucasbeckh@live.com>
2025-08-07 19:50:50 -0300

    1- Repository da classe usuário;

--- src/main/java/br/com/fumaca/repository/UsuarioRepository.java
+++ src/main/java/br/com/fumaca/repository/UsuarioRepository.java
@@ -4,6 +4,13 @@ import br.com.fumaca.model.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
